6b8c325b337dd364cd91dfa777c99cd75d8574ea
Swagat <swagatmitrabhattacharya18@gmail.com>
2024-01-12 22:21:26 +0530

    deleted file

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,14 +1,12 @@
 import Landing from "./components/Landing/Landing";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
-import Events from "./Pages/Events/Events";
 
 function App() {
 	return (
 		<div>
 			<Navbar />
 			<Landing />
-			<Events />
 			<Footer />
 		</div>
 	);
