4377e5298cc0a930e88a5844127825e4dfdbaf8c
RISHI RAJ SAHA <rishir_ug_22@ece.nits.ac.in>
2024-01-13 15:38:03 +0530

    Sponsor Card Add

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,3 +1,4 @@
 import Navbar from "./Navbar/Navbar";
 import Footer from "./Footer/Footer";
-export { Navbar, Footer };
+import Sponsor from "./Sponser/SponsorCard";
+export { Navbar, Footer, Sponsor };
