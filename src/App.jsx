b5b51d2fa8917328c661133d3cf7edf64bf198c9
Rishabh Ranjan <ranjan.rishabh0902@gmail.com>
2024-01-13 23:07:55 +0530

    ADD: Event Page Design

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import Home from './Pages/Home/Home'
+import EventPage from "./Pages/Home/EventPage/EventPage";
+import Home from "./Pages/Home/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route path='/' element={<Home />} />
+				<Route exact path="/" element={<Home />} />
+				<Route exact path="/Event" element={<EventPage />} />
 			</Routes>
 		</BrowserRouter>
 	);
