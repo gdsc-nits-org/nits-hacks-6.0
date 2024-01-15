9c252028e6678b4c79d2772b40e7f0405dda9db9
Rishav Bora <Rishavbora01@gmail.com>
2024-01-15 17:47:44 +0530

    past sponsors added

--- src/components/Sponsor/SponsorCard.jsx
+++ src/components/Sponsor/SponsorCard.jsx
@@ -3,32 +3,44 @@ const Sponsor = () => {
 	const sponsors = [
 		{
 			id: 1,
-			img: "https://res.cloudinary.com/dgnlmdkyq/image/upload/v1705059309/Nits%20hacks%206.0/Sponser/qy5qhfq0kludgcgebzin.svg",
+			img: "https://res.cloudinary.com/dxafdfvui/image/upload/v1705316692/interviewCake_1_kauyf0.svg",
 		},
 		{
 			id: 2,
-			img: "https://res.cloudinary.com/dgnlmdkyq/image/upload/v1705059309/Nits%20hacks%206.0/Sponser/lfu29ie3aklxwyjh88ya.svg",
+			img: "https://res.cloudinary.com/dxafdfvui/image/upload/v1705317303/campuskarma_qwkjzg.svg",
 		},
 		{
 			id: 3,
-			img: "https://res.cloudinary.com/dgnlmdkyq/image/upload/v1705059309/Nits%20hacks%206.0/Sponser/wtggcgvbsegp6t4e5mad.svg",
+			img: "https://res.cloudinary.com/dxafdfvui/image/upload/v1705318017/taskadenew_e3cr2l.svg",
 		},
 		{
 			id: 4,
-			img: "https://res.cloudinary.com/dgnlmdkyq/image/upload/v1705059308/Nits%20hacks%206.0/Sponser/byr8z9pbwsujrzkbbfbm.svg",
+			img: "https://res.cloudinary.com/dxafdfvui/image/upload/v1705318529/echo3d_almoq9.svg",
 		},
 		{
 			id: 5,
-			img: "https://res.cloudinary.com/dgnlmdkyq/image/upload/v1705059308/Nits%20hacks%206.0/Sponser/inwswvexnjhnjdeymzud.svg",
+			img: "https://res.cloudinary.com/dxafdfvui/image/upload/v1705319687/CryptoMode-Filecoin_hvwlug.svg",
 		},
 		{
 			id: 6,
-			img: "https://res.cloudinary.com/dgnlmdkyq/image/upload/v1705059308/Nits%20hacks%206.0/Sponser/wwipljdkdubocnhrl5iy.svg",
+			img: "https://res.cloudinary.com/dxafdfvui/image/upload/v1705319958/replit_tibgzp.svg",
 		},
+		{
+			id: 7,
+			img: "https://res.cloudinary.com/dxafdfvui/image/upload/v1705320298/solana_yhw3k5.svg",
+		},	
+		{
+			id: 8,
+			img: "https://res.cloudinary.com/dxafdfvui/image/upload/v1705320698/Polygon_blockchain_logo_gijj2n.svg",
+		},	
+		{
+			id: 9,
+			img: "https://res.cloudinary.com/dxafdfvui/image/upload/v1705320842/devfolio_jpfrru.svg",
+		}	
 	];
 	return (
 		<div className={styles.sponsorCont}>
-			<h1 className={styles.heading}>SPONSORS</h1>
+			<h1 className={styles.heading}>PAST SPONSORS</h1>
 			<div className={styles.pagebody}>
 				{sponsors.map((s) => {
 					return (
