1ccb80dddf6afb97dbc888978475432fd9930a6b
Rishabh Ranjan <ranjan.rishabh0902@gmail.com>
2024-01-15 10:05:14 +0530

    UPD: Created route for Event Page, Moved EventPage outside Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import { useState } from "react";
 import Home from "./Home";
 import Preview from "./components/Preview/Preview";
+import { Routes, Route } from "react-router-dom";
+import EventPage from "./Pages/EventPage/EventPage";
 
 const App = () => {
 	const [loaded, setLoaded] = useState(false);
-	return loaded ? <Home /> : <Preview setLoaded={setLoaded} />;
+	return (
+		<>
+			{loaded ? <Home /> : <Preview setLoaded={setLoaded} />};
+			<Routes>
+				<Route exact path="/Event" element={<EventPage />}></Route>
+			</Routes>
+		</>
+	);
 };
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,21 @@
-import React from "react";
+// import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.scss";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 // import Home from "./Home.jsx";
 // import Preview from "./Pages/Preview/Preview.jsx";
 
-const router = createBrowserRouter([
-	{
-		path: "/",
-		// element: <Preview/>
-		element: <App />,
-	},
-	//   {
-	//     path: "/home",
-	//     element: <Home />,
-	//   },
-]);
+// const router = createBrowserRouter([
+// 	{
+// 		path: "/",
+// 		// element: <Preview/>
+// 		element: <App />,
+// 	},
+// ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-	<React.StrictMode>
-		<RouterProvider router={router} />
-	</React.StrictMode>,
+	<BrowserRouter>
+		<App />
+	</BrowserRouter>,
 );
