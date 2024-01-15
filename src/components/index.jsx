22a0346e963ea211a2e641ab5571ff608ab154cf
Bijay Jiwrajka <jiwrajkabijay@gmail.com>
2024-01-15 13:37:54 +0530

    format code

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -5,4 +5,5 @@ import Landing from "./Landing/Landing";
 import Event from "./Event Card/EventCard";
 import NitsHacks from "./About/NitsHacks/NitsHacks";
 import Techno from "./About/Techno/Techno";
+
 export { Navbar, Footer, NitsHacks, Techno, Sponsor, Event, Landing };
