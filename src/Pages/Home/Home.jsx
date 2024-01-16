a78c9b23f77d108648ab128deda41b732be2b107
Partha_Pratim_Deka <Parthapratimdeka129@gmail.com>
2024-01-17 05:17:19 +0530

    ADDED: music

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -9,7 +9,16 @@ import {
 	Sponsor,
 	Event,
 } from "../../components/index";
+import { useState } from "react";
+import { useEffect } from "react";
 const Home = () => {
+	const [audio] = useState(new Audio("/sound/bgm.mp3"));
+
+	useEffect(() => {
+		audio.volume = 0.07;
+		audio.play();
+	});
+
 	return (
 		// <div style={{ overflowX: "hidden" }}>
 		<div>
