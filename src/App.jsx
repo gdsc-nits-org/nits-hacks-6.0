90e884fbebd9bf83886d4967058d399bcf774925
Partha Pratim Deka <79789738+parthaxntn@users.noreply.github.com>
2024-01-17 18:52:20 +0530

    Opening a PR for preview (#23)
    * Update: layerised the hero
    
    * Trying out the clouds
    
    * UPDATE: something cool
    
    * ADDED: music
    
    * Update: rain opacity
    
    * updated lockfile
    
    * pre-release
    
    * UPDATE: audio
    
    * UPDATE: Navbar transparency
    
    ---------
    
    Co-authored-by: byt3h3ad <adhirajd007@gmail.com>
    Co-authored-by: Swagat <swagatmitrabhattacharya18@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Home from "./Pages/Home/Home";
 import Preview from "./components/Preview/Preview";
 
 const App = () => {
 	const [loaded, setLoaded] = useState(false);
+	const [audio] = useState(new Audio("/sound/button2_re.mp3"));
+
+	useEffect(() => {
+		[...document.querySelectorAll("a")]
+			.filter((a) => !a.className.startsWith("_left"))
+			.forEach((e) => {
+				e.addEventListener("click", () => {
+					audio.play();
+				});
+			});
+	});
+
 	return loaded ? <Home /> : <Preview setLoaded={setLoaded} />;
 };
 export default App;
