994cf4711285d536701991855e071856aca30480
RISHI RAJ SAHA <132642857+rishinhk004@users.noreply.github.com>
2024-01-19 02:13:32 +0530

    bubble fixes (#28)
    * bubble fixex
    
    * fixes
    
    * fixes
    
    * fixes
    
    * Update Organisers.json
    
    * Update Organisers.json
    
    * Update Organisers.json
    
    * Update Organisers.json
    
    * Update Organisers.json
    
    * Update Tech.json

--- src/Pages/TeamPage/Carausel/Carausel.jsx
+++ src/Pages/TeamPage/Carausel/Carausel.jsx
@@ -13,74 +13,48 @@ import Coreteam from "../Coreteam.json";
 
 const MAX_VISIBILITY = 3;
 const MemberDetails = (d) => {
-	return <TeamCard {...d} key={d._id} />;
+    return <TeamCard {...d} key={d._id} />;
 };
 
 const Carousel = ({ children }) => {
-	const [active, setActive] = useState(0);
-	const count = React.Children.count(children);
-	return (
-		<div className={styles.carouselCont}>
-			<div className={styles.carousel}>
-				{active > 0 && (
-					<button
-						className={`${styles.nav} ${styles.left}`}
-						onClick={() => setActive((i) => i - 1)}
-					>
-						&lt;
-					</button>
-				)}
-				{React.Children.map(children, (child, i) => (
-					<div
-						className={styles.cardContainer}
-						style={{
-							"--active": i === active ? 1 : 0,
-							"--offset": (active - i) / 3,
-							"--direction": Math.sign(active - i),
-							"--abs-offset": Math.abs(active - i) / 3,
-							pointerEvents: active === i ? "auto" : "none",
-							opacity: Math.abs(active - i) >= MAX_VISIBILITY ? "0" : "1",
-							display: Math.abs(active - i) > MAX_VISIBILITY ? "none" : "block",
-							zIndex: `${i === active ? 5 : 0}`,
-						}}
-					>
-						{child}
-					</div>
-				))}
-				{active < count - 1 && (
-					<button
-						className={`${styles.nav} ${styles.right}`}
-						onClick={() => setActive((i) => i + 1)}
-					>
-						&gt;
-					</button>
-				)}
-			</div>
+    const [active, setActive] = useState(0);
+    const count = React.Children.count(children);
+    return (
+        <div className={styles.carouselCont}>
+            <div className={styles.carousel}>
+                {active > 0 && <button className={styles.left} onClick={() => setActive(i => i - 1)}>&lt;</button>}
+                {React.Children.map(children, (child, i) => (
+                    <div className={styles.cardContainer} style={{
+                        '--active': i === active ? 1 : 0,
+                        '--offset': (active - i) / 3,
+                        '--direction': Math.sign(active - i),
+                        '--abs-offset': Math.abs(active - i) / 3,
+                        'pointerEvents': active === i ? 'auto' : 'none',
+                        'opacity': Math.abs(active - i) >= MAX_VISIBILITY ? '0' : '1',
+                        'display': Math.abs(active - i) > MAX_VISIBILITY ? 'none' : 'block',
+                        zIndex: `${i === active ? 5 : 0}`,
 
-			<div
-				style={{
-					display: "flex",
-					alignItems: "center",
-					justifyContent: "center",
-				}}
-			>
-				<div className={`${styles.bubbleCont}`}>
-					{React.Children.map(children, (child, i) => (
-						<div
-							className={
-								i === active ? `${styles.active}` : `${styles.inactive}`
-							}
-						></div>
-					))}
-				</div>
-			</div>
-		</div>
-	);
+                    }}>
+                        {child}
+                    </div>
+                ))}
+                {active < count - 1 && <button className={styles.right} onClick={() => setActive(i => i + 1)}>&gt;</button>}
+            </div>
+
+            <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                <div className={`${styles.bubbleCont}`}>
+                    {React.Children.map(children, (child, i) => (
+                        <div className={i === active ? `${styles.active}` : `${styles.inactive}`}></div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
 };
 
 const Events = () => (
-	<div className={styles.page}>
-		{/*}     <div className={styles.content}>
+    <div className={styles.page}>
+        {/*}     <div className={styles.content}>
             <div className={styles.heading}><p>FACULTY</p></div>
             <div className={styles.Events}>
                 <Carousel>
@@ -90,48 +64,48 @@ const Events = () => (
             </div>
                 </div> 
             */}
-		<div className={styles.content}>
-			<div className={styles.heading}>
-				<p>CORE</p>
-			</div>
+        <div className={styles.content}>
+            <div className={styles.heading}>
+                <p>CORE</p>
+            </div>
 
-			<div className={styles.Events}>
-				<Carousel>{Convener.map(MemberDetails)}</Carousel>
-			</div>
-			<div className={styles.Events}>
-				<Carousel>{Coordinator.map(MemberDetails)}</Carousel>
-			</div>
-			<div className={styles.Events}>
-				<Carousel>{Coreteam.map(MemberDetails)}</Carousel>
-			</div>
-		</div>
-		<div className={styles.content}>
-			<div className={styles.heading}>
-				<p>TECH</p>
-			</div>
-			<div className={styles.Events}>
-				<Carousel>{Techlead.map(MemberDetails)}</Carousel>
-			</div>
-			<div className={styles.Events}>
-				<Carousel>{TECH.map(MemberDetails)}</Carousel>
-			</div>
-		</div>
-		<div className={styles.content}>
-			<div className={styles.heading}>
-				<p>ORGANISERS</p>
-			</div>
-			<div className={styles.Events}>
-				<Carousel>{ORGANISERS.map(MemberDetails)}</Carousel>
-			</div>
-		</div>
-		<div className={styles.content}>
-			<div className={styles.heading}>
-				<p>VOLUNTEERS</p>
-			</div>
-			<div className={styles.Events}>
-				<Carousel>{VOLUNTEERS.map(MemberDetails)}</Carousel>
-			</div>
-		</div>
-	</div>
+            <div className={styles.Events}>
+                <Carousel>{Convener.map(MemberDetails)}</Carousel>
+            </div>
+            <div className={styles.Events}>
+                <Carousel>{Coordinator.map(MemberDetails)}</Carousel>
+            </div>
+            <div className={styles.Events}>
+                <Carousel>{Coreteam.map(MemberDetails)}</Carousel>
+            </div>
+        </div>
+        <div className={styles.content}>
+            <div className={styles.heading}>
+                <p>TECH</p>
+            </div>
+            <div className={styles.Events}>
+                <Carousel>{Techlead.map(MemberDetails)}</Carousel>
+            </div>
+            <div className={styles.Events}>
+                <Carousel>{TECH.map(MemberDetails)}</Carousel>
+            </div>
+        </div>
+        <div className={styles.content}>
+            <div className={styles.heading}>
+                <p>ORGANISERS</p>
+            </div>
+            <div className={styles.Events}>
+                <Carousel>{ORGANISERS.map(MemberDetails)}</Carousel>
+            </div>
+        </div>
+        <div className={styles.content}>
+            <div className={styles.heading}>
+                <p>VOLUNTEERS</p>
+            </div>
+            <div className={styles.Events}>
+                <Carousel>{VOLUNTEERS.map(MemberDetails)}</Carousel>
+            </div>
+        </div>
+    </div>
 );
 export default Events;
