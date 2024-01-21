65f70b0bc9b6664ea91adfb38d42c2722432e727
Swagat <swagatmitrabhattacharya18@gmail.com>
2024-01-21 14:18:13 +0530

    link change

--- src/components/SpeakerCard/SpeakerCard.jsx
+++ src/components/SpeakerCard/SpeakerCard.jsx
@@ -4,14 +4,16 @@ const SpeakerCard = () => {
 	const speakers = [
 		{
 			id: 1,
-			image: "/utkarsh.jpg",
+			image:
+				"https://res.cloudinary.com/dnuhxx8aa/image/upload/v1705826342/utkarsh_ekhwgs.webp",
 			name: "Utkarsh Gupta",
 			role: "SDE @Graviton",
 			data: "AIR 1 @Google Kickstart; Ex AIR 1 @Codeforces; Grandmaster @Codeforces; 7 Star on CodeChef; ICPC '21 World Finalist",
 		},
 		{
 			id: 2,
-			image: "/harsh.jpg",
+			image:
+				"https://res.cloudinary.com/dnuhxx8aa/image/upload/v1705826158/harsh_mu4xlo.webp",
 			name: "Harsh Mishra",
 			role: "Open Source Engineer @LocalStack",
 			data: "Google Summer of Code '21; Google Season of Docs '21; AWS Community Builder; Open Source Contributor @moja global",
