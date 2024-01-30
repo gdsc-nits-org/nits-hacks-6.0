7fee06d312a3155447805def5e4a9f07035e6177
Rishabh Ranjan <ranjan.rishabh0902@gmail.com>
2024-01-30 23:14:48 +0530

    UPD:Adeed questions with answer,Made some responsive changes. LEFT:Adding Arrow

--- src/Pages/FAQPage/FaqPage.jsx
+++ src/Pages/FAQPage/FaqPage.jsx
@@ -11,45 +11,136 @@ const FaqPage = () => {
 					src="https://res.cloudinary.com/dvra8rj4n/image/upload/v1706544920/NITSX/aqtbggdpooofxlccyxxf.png"
 					alt=""
 				/>
-				<ul className={styles.accordion}>
-					<li>
-						<input type="checkbox" name="accordion" id="1" />
-						<label className={styles.label} htmlFor="1">
-							Q. Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus
-							reprehenderit sapiente quod veritatis consequatur
-						</label>
+				<div className={styles.faq_wrapper}>
+					<ul className={styles.accordion}>
+						<li>
+							<input type="checkbox" name="accordion" id="1" />
+							<label className={styles.label} htmlFor="1">
+								Q. Will the events be online or offline?
+							</label>
 
-						<div className={styles.content}>
-							<span>
-								A. Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-								Adipisci, suscipit tempora molestiae laboriosam praesentium
-								ipsum iure distinctio ipsam accusamus quam quisquam aliquam
-								itaque ratione cupiditate sint. Magnam sed sunt perferendis,
-								exercitationem atque id cumque sapiente nihil praesentium
-								deleniti officiis facere alias ratione, ullam asperiores harum
-								hic aperiam accusamus sit assumenda veniam! Dolorem nulla
-								voluptate officiis!
-							</span>
-						</div>
-					</li>
-				</ul>
-				<ul className={styles.accordion}>
-					<li>
-						<input type="checkbox" name="accordion" id="2" />
-						<label className={styles.label} htmlFor="2">
-							Q. Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus
-							reprehenderit sapiente quod veritatis consequatur
-						</label>
-						<div className={styles.content}>
-							<span>
-								A. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-								Corrupti dicta officiis itaque magnam eveniet aspernatur.
-								Tenetur voluptatum qui eaque earum officia placeat, culpa totam
-								sit repellat nesciunt architecto unde quidem!
-							</span>
-						</div>
-					</li>
-				</ul>
+							<div className={styles.content}>
+								<span>
+									A. Software Track - Offline <br />
+									&nbsp;&nbsp;&nbsp;UI/UX Track and Coding Track - Hybrid
+								</span>
+							</div>
+						</li>
+					</ul>
+
+					<ul className={styles.accordion}>
+						<li>
+							<input type="checkbox" name="accordion" id="2" />
+							<label className={styles.label} htmlFor="2">
+								Q. What is the minimum number of members in each team?
+							</label>
+
+							<div className={styles.content}>
+								<span>
+									A. Coding and Software - 2 <br />
+									&nbsp;&nbsp;&nbsp;UI/UX - Individual
+								</span>
+							</div>
+						</li>
+					</ul>
+
+					<ul className={styles.accordion}>
+						<li>
+							<input type="checkbox" name="accordion" id="3" />
+							<label className={styles.label} htmlFor="3">
+								Q. Are there any participation fees?
+							</label>
+
+							<div className={styles.content}>
+								<span>A. No participation fees is needed.</span>
+							</div>
+						</li>
+					</ul>
+
+					<ul className={styles.accordion}>
+						<li>
+							<input type="checkbox" name="accordion" id="4" />
+							<label className={styles.label} htmlFor="4">
+								Q. Can students of other colleges participate remotely?
+							</label>
+
+							<div className={styles.content}>
+								<span>
+									A. UI/UX and Coding Track - Remote participation allowed.
+								</span>
+							</div>
+						</li>
+					</ul>
+
+					<ul className={styles.accordion}>
+						<li>
+							<input type="checkbox" name="accordion" id="5" />
+							<label className={styles.label} htmlFor="5">
+								Q. Do we need to be very good in order to participate?
+							</label>
+
+							<div className={styles.content}>
+								<span>
+									A. You need the basic knowledge of HTML, CSS to participate in
+									Software track. That is more than enough. <br /> <br />
+									Basic programming knowledge in any language is enough for
+									Coding track. <br /> <br />
+									Any sort of design thinking is welcome to participate in
+									UI/UX. You need not be an expert in it.
+								</span>
+							</div>
+						</li>
+					</ul>
+
+					<ul className={styles.accordion}>
+						<li>
+							<input type="checkbox" name="accordion" id="6" />
+							<label className={styles.label} htmlFor="6">
+								Q. What are the eligibility criteria for participation?
+							</label>
+
+							<div className={styles.content}>
+								<span>
+									A. Anyone with the interest to learn is free to participate.
+									No one is restricted from participating.
+								</span>
+							</div>
+						</li>
+					</ul>
+
+					<ul className={styles.accordion}>
+						<li>
+							<input type="checkbox" name="accordion" id="7" />
+							<label className={styles.label} htmlFor="7">
+								Q. Are there any restrictions on the tools or technologies
+								participants can use?
+							</label>
+
+							<div className={styles.content}>
+								<span>
+									A. Participants are free to use any programming languages,
+									frameworks, or tools that they prefer, as long as they meet
+									the requirements of the respective tracks.
+								</span>
+							</div>
+						</li>
+					</ul>
+
+					<ul className={styles.accordion}>
+						<li>
+							<input type="checkbox" name="accordion" id="8" />
+							<label className={styles.label} htmlFor="8">
+								Q. Will there be prizes for the winners?
+							</label>
+
+							<div className={styles.content}>
+								<span>
+									A. There are perks for both winners and participants.
+								</span>
+							</div>
+						</li>
+					</ul>
+				</div>
 			</div>
 			<Footer />
 		</>
